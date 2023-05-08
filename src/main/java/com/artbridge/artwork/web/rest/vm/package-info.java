d4c27e36eac4fd7dcf5158a8b17092d1f02f512e
ImKunYoung <ur2kunyoung2@outlook.com>
2023-05-08 22:25:33 +0900

    Initial version of Artwork generated by generator-jhipster@7.9.3

---
+++ src/main/java/com/artbridge/artwork/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.artbridge.artwork.web.rest.vm;
---
+++ src/test/java/com/artbridge/artwork/IntegrationTest.java
@@ -0,0 +1,24 @@
+package com.artbridge.artwork;
+
+import com.artbridge.artwork.ArtworkApp;
+import com.artbridge.artwork.config.AsyncSyncConfiguration;
+import com.artbridge.artwork.config.EmbeddedKafka;
+import com.artbridge.artwork.config.EmbeddedSQL;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { ArtworkApp.class, AsyncSyncConfiguration.class })
+@EmbeddedKafka
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
