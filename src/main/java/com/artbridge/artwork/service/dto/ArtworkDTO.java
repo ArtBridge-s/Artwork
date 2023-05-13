0d48430c756c19d5ad829c960492d8f8f23eb8aa
imkunyoung <lim8667@gmail.com>
2023-05-13 16:36:34 +0900

    refactor: Refactor code to add 'member' field to Artwork class

--- src/main/java/com/artbridge/artwork/service/dto/ArtworkDTO.java
+++ src/main/java/com/artbridge/artwork/service/dto/ArtworkDTO.java
@@ -2,7 +2,6 @@ package com.artbridge.artwork.service.dto;
 
 import com.artbridge.artwork.domain.enumeration.Status;
 import java.io.Serializable;
-import java.util.Objects;
 import lombok.Data;
 
 /**
@@ -13,58 +12,13 @@ import lombok.Data;
 public class ArtworkDTO implements Serializable {
 
     private Long id;
-
     private String title;
-
     private String shortDescription;
-
     private String longDescription;
-
     private String imageUrl;
-
     private String artistname;
-
     private String makingday;
-
+    private MemberDTO member;
     private Status status;
 
-    public Long getId() {
-        return id;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof ArtworkDTO)) {
-            return false;
-        }
-
-        ArtworkDTO artworkDTO = (ArtworkDTO) o;
-        if (this.id == null) {
-            return false;
-        }
-        return Objects.equals(this.id, artworkDTO.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(this.id);
-    }
-
-    // prettier-ignore
-    @Override
-    public String toString() {
-        return "ArtworkDTO{" +
-            "id=" + getId() +
-            ", title='" + getTitle() + "'" +
-            ", shortDescription='" + getShortDescription() + "'" +
-            ", longDescription='" + getLongDescription() + "'" +
-            ", imageUrl='" + getImageUrl() + "'" +
-            ", artistname='" + getArtistname() + "'" +
-            ", makingday='" + getMakingday() + "'" +
-            ", status='" + getStatus() + "'" +
-            "}";
-    }
 }
