6a26b6e181be2212d49a4a7de4450d68a678f349
imkunyoung <lim8667@gmail.com>
2023-05-13 17:10:52 +0900

    refactor: Remove unused @SuppressWarnings("common-java:DuplicatedBlocks") annotation from ViewDTO class.

--- src/main/java/com/artbridge/artwork/service/dto/ViewDTO.java
+++ src/main/java/com/artbridge/artwork/service/dto/ViewDTO.java
@@ -1,7 +1,6 @@
 package com.artbridge.artwork.service.dto;
 
 import java.io.Serializable;
-import java.util.Objects;
 import lombok.Data;
 
 /**
@@ -17,34 +16,4 @@ public class ViewDTO implements Serializable {
 
     private ArtworkDTO artwork;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof ViewDTO)) {
-            return false;
-        }
-
-        ViewDTO viewDTO = (ViewDTO) o;
-        if (this.id == null) {
-            return false;
-        }
-        return Objects.equals(this.id, viewDTO.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(this.id);
-    }
-
-    // prettier-ignore
-    @Override
-    public String toString() {
-        return "ViewDTO{" +
-            "id=" + getId() +
-            ", member=" + getMember() +
-            ", artwork=" + getArtwork() +
-            "}";
-    }
 }
