e4d2508a87dff6c39c20777c6974522f46f33c58
ImKunYoung <ur2kunyoung2@outlook.com>
2023-05-21 18:44:34 +0900

    feat: Implement GCSService for uploading images to Google Cloud Storage.

--- src/main/java/com/artbridge/artwork/adaptor/GCSService.java
+++ src/main/java/com/artbridge/artwork/adaptor/GCSService.java
@@ -5,5 +5,5 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 public interface GCSService {
-    String uploadFileToGCS(MultipartFile file) throws IOException;
+    String uploadImageToGCS(MultipartFile imageFile) throws IOException;
 }
