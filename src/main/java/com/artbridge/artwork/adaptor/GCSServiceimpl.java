a1661b6965b56d9eec30db19bf0a512f37a7ddce
ImKunYoung <ur2kunyoung2@outlook.com>
2023-05-21 18:44:34 +0900

    feat: Implement GCSService for uploading images to Google Cloud Storage.

--- src/main/java/com/artbridge/artwork/adaptor/GCSServiceimpl.java
+++ src/main/java/com/artbridge/artwork/adaptor/GCSServiceimpl.java
@@ -1,44 +1,42 @@
 package com.artbridge.artwork.adaptor;
 
-import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.storage.BlobId;
 import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Storage;
-import com.google.cloud.storage.StorageOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.UUID;
 
 @Service
 public class GCSServiceimpl implements GCSService {
+
+    private final String bucketName;
+    private final Storage storage;
+
+    public GCSServiceimpl(@Value("${spring.cloud.gcp.storage.bucket}") String bucketName, Storage storage) {
+        this.bucketName = bucketName;
+        this.storage = storage;
+    }
+
     @Override
-    public String uploadFileToGCS(MultipartFile file) throws IOException {
-        String bucketName = "artbridge-bucket";
+    public String uploadImageToGCS(MultipartFile imageFile) throws IOException {
+        String objectName = generateRandomObjectName();
 
-        // Initialize a Cloud Storage client
-        Storage storage = StorageOptions.newBuilder()
-            .setCredentials(GoogleCredentials.fromStream(new FileInputStream("/artwork")))
-            .build()
-            .getService();
-
-        // Convert MultipartFile to java.nio.file.Path
-        Path tempFile = Files.createTempFile("temp", file.getOriginalFilename());
-        file.transferTo(tempFile);
-
-        // Generate a random objectName
-        String objectName = UUID.randomUUID().toString();
-
-        // Upload the file to Cloud Storage
         BlobId blobId = BlobId.of(bucketName, objectName);
-        BlobInfo blobInfo = BlobInfo.newBuilder(blobId).build();
-        storage.create(blobInfo, Files.readAllBytes(tempFile));
+        BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setContentType("image/jpeg").build();
+        storage.create(blobInfo, imageFile.getBytes());
 
-        // Return the public download URL
+        return generateDownloadURL(objectName);
+    }
+
+    private String generateRandomObjectName() {
+        return UUID.randomUUID() + ".jpg";
+    }
+
+    private String generateDownloadURL(String objectName) {
         return String.format("https://storage.googleapis.com/%s/%s", bucketName, objectName);
     }
 }
