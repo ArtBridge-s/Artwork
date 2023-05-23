ed9aeadb9b81294156198d71a7bffb5e30fda585
ImKunYoung <ur2kunyoung2@outlook.com>
2023-05-23 11:07:17 +0900

    feat: Remove unused imports in ArtworkRepository.

--- src/main/java/com/artbridge/artwork/repository/ArtworkRepository.java
+++ src/main/java/com/artbridge/artwork/repository/ArtworkRepository.java
@@ -1,12 +1,22 @@
 package com.artbridge.artwork.repository;
 
 import com.artbridge.artwork.domain.Artwork;
+import com.artbridge.artwork.domain.enumeration.Status;
+import com.google.common.io.Files;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 /**
  * Spring Data JPA repository for the Artwork entity.
  */
 @SuppressWarnings("unused")
 @Repository
-public interface ArtworkRepository extends JpaRepository<Artwork, Long> {}
+public interface ArtworkRepository extends JpaRepository<Artwork, Long> {
+
+    Page<Artwork> findAllByStatus(Status status, Pageable pageable);
+
+}
