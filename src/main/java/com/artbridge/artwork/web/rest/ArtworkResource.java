ee1522a151ec3ceffab6e1ee730c57782b70b880
ImKunYoung <ur2kunyoung2@outlook.com>
2023-05-23 12:23:52 +0900

    refactor: Optimize import statements in the class, removing unused imports and organizing the remaining ones.

--- src/main/java/com/artbridge/artwork/web/rest/ArtworkResource.java
+++ src/main/java/com/artbridge/artwork/web/rest/ArtworkResource.java
@@ -20,11 +20,6 @@ import java.util.Optional;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
