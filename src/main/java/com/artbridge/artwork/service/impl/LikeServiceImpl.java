880cade50aa04468adba95a81fb544a613087913
ImKunYoung <ur2kunyoung2@outlook.com>
2023-05-24 11:34:36 +0900

    feat(DSLY-18): Improve efficiency of countByPostId method in LikeService implementation

--- src/main/java/com/artbridge/artwork/service/impl/LikeServiceImpl.java
+++ src/main/java/com/artbridge/artwork/service/impl/LikeServiceImpl.java
@@ -82,4 +82,9 @@ public class LikeServiceImpl implements LikeService {
         log.debug("Request to delete Like : {}", id);
         likeRepository.deleteById(id);
     }
+
+    @Override
+    public Long countByPostId(Long artworkId) {
+        return likeRepository.countByArtwork_Id(artworkId);
+    }
 }
