edbbc5e5dee279d2227df5d14ebee3a5d677d8d6
ImKunYoung <ur2kunyoung2@outlook.com>
2023-05-24 10:33:59 +0900

    docs(DSLY-12): Update API documentation for retrieving a page of artworks in delete pending status.

--- src/main/java/com/artbridge/artwork/service/ArtworkService.java
+++ src/main/java/com/artbridge/artwork/service/ArtworkService.java
@@ -72,6 +72,7 @@ public interface ArtworkService {
      */
     Page<ArtworkDTO> findCreatePendings(Pageable pageable);
 
+
     /**
      * 업데이트 대기 중인 Artwork 목록을 페이지별로 조회합니다.
      *
@@ -80,6 +81,13 @@ public interface ArtworkService {
      */
     Page<ArtworkDTO> findUpdatePendings(Pageable pageable);
 
+
+    /**
+     * 삭제 대기 중인 Artwork 목록을 페이지별로 조회합니다.
+     *
+     * @param pageable 페이지 정보 (Pageable)
+     * @return 페이지별로 조회된 삭제 대기 중인 Artwork 목록 (Page 객체)
+     */
     Page<ArtworkDTO> findDeletePendings(Pageable pageable);
 
 
