7cb5a30d1d82d33e2bfad92de2594a3a909bd178
ImKunYoung <ur2kunyoung2@outlook.com>
2023-06-01 18:18:08 +0900

    fix(#45): Update security configuration to allow public access to specific endpoints

--- src/main/java/com/artbridge/artwork/config/SecurityConfiguration.java
+++ src/main/java/com/artbridge/artwork/config/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import com.artbridge.artwork.security.*;
 import com.artbridge.artwork.security.jwt.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -49,6 +50,7 @@ public class SecurityConfiguration {
             .and()
             .authorizeRequests()
             .antMatchers("/h2-console/**").permitAll()
+            .antMatchers(HttpMethod.GET,"/api/artworks/**").permitAll()
             .antMatchers("/api/authenticate").permitAll()
             .antMatchers("/api/admin/**").hasAuthority(AuthoritiesConstants.ADMIN)
             .antMatchers("/api/**").authenticated()
