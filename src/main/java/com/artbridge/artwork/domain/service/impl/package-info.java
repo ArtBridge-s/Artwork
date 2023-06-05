a4e2514df9fe3066864dafb280f3e00bce36d21a
ImKunYoung <ur2kunyoung2@outlook.com>
2023-06-05 17:18:11 +0900

    refactor: Update domain.service

---
+++ src/main/java/com/artbridge/artwork/domain/service/impl/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * @packageName : com.artbridge.artwork.domain.service.impl
+ * @fileName : package-info
+ * @author : ImKunYoung
+ * @since : 2023-06-05
+ * description    :
+ * <p>
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023-06-05        ImKunYoung       최초 생성
+ */
+package com.artbridge.artwork.domain.service.impl;
