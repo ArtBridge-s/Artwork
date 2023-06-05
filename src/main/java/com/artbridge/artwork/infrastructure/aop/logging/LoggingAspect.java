580371c999e09b6faf8e512686783c11ff5dc5fd
ImKunYoung <ur2kunyoung2@outlook.com>
2023-06-05 18:35:06 +0900

    style: Remove Blank

--- src/main/java/com/artbridge/artwork/infrastructure/aop/logging/LoggingAspect.java
+++ src/main/java/com/artbridge/artwork/infrastructure/aop/logging/LoggingAspect.java
@@ -15,7 +15,6 @@ import tech.jhipster.config.JHipsterConstants;
 
 /**
  * Aspect for logging execution of service and repository Spring components.
- *
  * By default, it only runs with the "dev" profile.
  */
 @Aspect
